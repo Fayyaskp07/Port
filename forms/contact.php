74e765b3c0bb648d5fa370c82a61cf80d081f1dc
Fayyaskp07 <fayyaskp10@gmail.com>
2026-09-07 22:49:16 +0530

    Add files via upload

---
+++ forms/contact.php
@@ -0,0 +1,63 @@
+<?php
+  /**
+  * Requires the "PHP Email Form" library
+  * The "PHP Email Form" library is available only in the pro version of the template
+  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
+  * For more info and help: https://bootstrapmade.com/php-email-form/
+  */
+
+  // Replace contact@example.com with your real receiving email address
+ 
+ 
+ /* $receiving_email_address = 'contact@example.com';
+
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
+
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
+
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  /*
+  $contact->smtp = array(
+    'host' => 'example.com',
+    'username' => 'example',
+    'password' => 'pass',
+    'port' => '587'
+  );
+  */
+
+ /* $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
+
+  echo $contact->send();
+?>*/
+
+
+/*<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get data from form
+    $name = htmlspecialchars($_POST["name"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $subject = htmlspecialchars($_POST["subject"]);
+    $message = htmlspecialchars($_POST["message"]);
+
+    // Optional: You can print it on screen for testing
+    echo "Message received from <strong>$name</strong> (<em>$email</em>)<br>";
+    echo "<strong>Subject:</strong> $subject<br>";
+    echo "<strong>Message:</strong><br>$message";
+} else {
+    // If not POST, return 405 error
+    http_response_code(405);
+    echo "405 Method Not Allowed";
+}
+?>
